e89b600988d10d309d8e5e4f4bc7b4d22c8e7976
Khalil Estell <kammce0@gmail.com>
2025-05-27 10:25:23 -0700

    :recycle: Migrate to `libhal-bootstrap.app`

--- conanfile.py
+++ conanfile.py
@@ -17,9 +17,9 @@ from conan import ConanFile
 required_conan_version = ">=2.2.2"
 
 
-class demos(ConanFile):
+class LibhalApplication(ConanFile):
     python_requires = "libhal-bootstrap/[^4.2.1]"
-    python_requires_extend = "libhal-bootstrap.demo"
+    python_requires_extend = "libhal-bootstrap.app"
 
     def requirements(self):
         bootstrap = self.python_requires["libhal-bootstrap"]
